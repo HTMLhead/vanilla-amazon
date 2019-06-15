6a7f7f7dca56284c41adaea0da48bb429b0a4888
HTMLhead <mrdoo309@gmail.com>
2019-06-15 22:13:27 +0900

    update(babel): 바벨, 웹팩 설정 추가 및 실험

---
+++ src/amazonJS/app.js
@@ -0,0 +1,54 @@
+import { ScrollEventSticky } from "./scrollEvent-Sticky.js";
+import { CarouselMiddle } from "./carousel-middle.js";
+import { SearchAutocorrect } from "./search-autocorrect.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollEventSticky = new ScrollEventSticky({
+    header: "header",
+    stickyLayer: ".top-layer-container",
+    hiddenLayer: ".prime-member-container",
+    closeBtn: ".close-button",
+    closeArrowBtn: ".close-button-foot",
+    openArrowBtn: ".top-layer-trigger-button",
+    topLayerActiveClass: "top-layer-active",
+    scrollActiveClass: "prime-member-container-scroll-active",
+    btnActiveClass: "prime-member-container-active",
+  });
+
+  const carouselMiddle = new CarouselMiddle(
+    {
+      container: ".middle-body-carousel-list",
+      rightBtn: ".middle-body-carousel-right-button",
+      leftBtn: ".middle-body-carousel-left-button",
+      anchorEl: ".middle-body-carousel-url",
+    },
+    {
+      ajaxDataUrl: "./jsonData/data.json",
+    },
+    {
+      carouselSize: "230px",
+      carouselAutoMovingMS: 3000,
+      autoEventStopContainer: ".middle-body-carousel",
+      transitionTime: "0.1s",
+      transitionPart: "all",
+    }
+  );
+
+  const searchAutocorrect = new SearchAutocorrect(
+    {
+      searchWindow: ".head-search-blank",
+      autocorrectWindow: ".head-search-autocorrect-container",
+      autocorrectListsClass: "head-search-autocorrect-list", //not class
+      toBeCloakedElement: ".wholeBodyContainer",
+      searchButtonElement: ".head-search-button",
+    },
+    {
+      fetchUrl: "http://localhost:8000/",
+    },
+    {
+      cloakingTransitionTime: "0.3s",
+      shadingAutocorrectClass: "shade",
+      cloakingBackgroundClass: "cloaking",
+    }
+  );
+});
